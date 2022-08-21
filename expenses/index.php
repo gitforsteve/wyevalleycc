ac399fe5ca6f585bc29dea898e0a084778f75f86
Steve Eans <steve@parva.co.uk>
2022-08-21 15:53:39 +0100

    Validation

--- expenses/index.php
+++ expenses/index.php
@@ -28,13 +28,88 @@
             }    
             fieldset {
                   background-color: #eeeeee;
+                  padding: 5px;
             }
             legend {
                 background-color: gray;
                 color: white;
                 padding: 5px 10px;
             }
-         </style>
+            input.error {
+                border: 2px solid red;
+            }
+            textarea.error {
+                border: 2px solid red;
+            }
+        </style>
+        <script src="../js/jquery-3.1.1.min.js"></script>
+        <script src="../js/jquery-ui.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                $('#date').datepicker({
+                    dateFormat: "d MM yy"
+                });
+                $('#printform').on("click",function(){
+                    ok = true;
+                    $('#name').removeClass('error');
+                    $('#date').removeClass('error');
+                    $('#fares').removeClass('error');
+                    if(!$.trim($('#name').val())){
+                        $('#name').addClass('error');
+                        $('#name').attr('placeholder','YOUR NAME IS REQUIRED');
+                        ok = false;
+                    }
+                    if(!$.trim($('#date').val())){
+                        $('#date').addClass('error');
+                        $('#date').attr('placeholder','THE DATE IS REQUIRED')
+                        ok = false;
+                    }
+                    if(!$.trim($('#description').val())){
+                        $('#description').addClass('error');
+                        $('#description').attr('placeholder','REASON FOR EXPENSE IS REQUIRED');
+                        ok = false;
+                    }
+                   if(ok){
+                    $('#expform').submit();
+                   }
+                })
+                $('#fares').on('input',function(){
+                    if($.trim($(this).val()) && $(this).val() > 0){
+                        $('#farerec').prop('checked',true);
+                    }else{
+                        $('#farerec').prop('checked',false);
+                    }
+                });
+                $('#parking').on('input',function(){
+                    if($.trim($(this).val()) && $(this).val() > 0){
+                        $('#parkingrec').prop('checked',true);
+                    }else{
+                        $('#parkingrec').prop('checked',false);
+                    }
+                });
+                $('#accom').on('input',function(){
+                    if($.trim($(this).val()) && $(this).val() > 0){
+                        $('#accomrec').prop('checked',true);
+                    }else{
+                        $('#accomrec').prop('checked',false);
+                    }
+                });
+                $('#food').on('input',function(){
+                    if($.trim($(this).val()) && $(this).val() > 0){
+                        $('#foodrec').prop('checked',true);
+                    }else{
+                        $('#foodrec').prop('checked',false);
+                    }
+                });
+                $('#other').on('input',function(){
+                    if($.trim($(this).val()) && $(this).val() > 0){
+                        $('#otherrec').prop('checked',true);
+                    }else{
+                        $('#otherrec').prop('checked',false);
+                    }
+                });
+            });
+        </script>
     </head>
     <body>
         <!--[if lt IE 7]>
@@ -43,31 +118,32 @@
         <div class="container">
             <h1 style="text-align:center;">Wye Valley Community Council</h1>
             <h2 style="text-align:center;">Expenses Claim</h2>
-            <form action="expenses.php" method="POST" autocomplete="off">
+            <form id="expform" action="expenses.php" method="POST" autocomplete="off">
                 <div class="row">
-                    <div class="six columns">
-                        <label for="name">Your name</label>
-                        <input type="text" list="names" class="u-full-width" name="name" id="name" />
-                        <datalist id="names">
-                            <?PHP
-                            var_dump($GLOBALS);
+                    <fieldset>
+                        <legend>Claim details</legend>
+                        <div class="six columns">
+                            <label for="name">Your name</label>
+                            <input type="text" list="names" class="u-full-width" name="name" id="name" />
+                            <datalist id="names">
+                                <?PHP
                             foreach($councillors as $councillor){
-                                printf("<option value='%s %s'>",$councillor->name,$councillor->surname);
-                            }
-                            ?>
-                        </datalist>
+                                    printf("<option value='%s %s'>",$councillor->name,$councillor->surname);
+                                }
+                                ?>
+                            </datalist>
+                        </div>
+                        <div class="five columns">
+                            <label for="date">Date of expense</label>
+                            <input type="text" class="u-full-width" name="date" id="date" />
+                        </div>
+                        <div class="row">
+                        <div class="u-full-width">
+                            <label for="description">Reason for expense</label>
+                            <textarea class="u-full-width" name="description" id="description"></textarea>
+                        </div>
                     </div>
-                    <div class="six columns">
-                        <label for="date">Date of expense</label>
-                        <input type="text" class="u-full-width" name="date" id="date" />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="u-full-width">
-                        <label for="description">Reason for expense</label>
-                        <textarea class="u-full-width" name="description" id="description"></textarea>
-                    </div>
-                </div>
+                </fieldset>
                 <div class="row">
                     <fieldset>
                         <legend>Travel</legend>
@@ -75,7 +151,7 @@
                         <div class="four columns">
                             <label for="fares">Fares</label>
                             <input type="number" step="0.01" name="fares" id="fares" class="u-full-width" placeholder="Receipts required">
-                            <input type="checkbox" name="farerec"> Receipts attached
+                            <input type="checkbox" name="farerec" id="farerec"> <span id="farerecspan">Receipts attached</span>
                         </div>
                         <div class="three columns">
                             <label for="mileage">Vehicle mileage</label>
@@ -84,7 +160,7 @@
                         <div class="four columns">
                             <label for="parking">Parking fees</label>
                             <input type="number" step="0.01" name="parking" id="parking" class="u-full-width" placeholder="Receipt/ticket required">
-                            <input type="checkbox" name="parkingrec"> Receipts attached
+                            <input type="checkbox" name="parkingrec" id="parkingrec"> Receipts attached
                         </div>
                     </fieldset>
                 </div>
@@ -95,13 +171,13 @@
                             <label for="accom">Accomodation</label>
                             <input type="number" step="0.01" name="accom" id="accom" class="u-full-width" placeholder="Receipt required">
                             <br />
-                            <input type="checkbox" name="accomrec"> Receipts attached
+                            <input type="checkbox" name="accomrec" id="accomrec"> Receipts attached
                         </div>
                         <div class="four columns">
                             <label for="food">Food and drink</label>
                             <input type="number" step="0.01" name="food" id="food" class="u-full-width" placeholder="Receipt required">
                             <br />
-                            <input type="checkbox" name="foodrec"> Receipt attached
+                            <input type="checkbox" name="foodrec" id="foodrec"> Receipts attached
                         </div>
                     </fieldset>
                 </div>
@@ -116,7 +192,7 @@
                             <label for="other">Amount paid</label>
                             <input type="number" step="0.01" name="other" id="other" class="u-full-width" placeholder="Receipt if available">
                             <br />
-                            <input type="checkbox" name="otherrec"> Receipt attached
+                            <input type="checkbox" name="otherrec" id="otherrec"> Receipts attached
                         </div>
                     </fieldset>
                 </div>
@@ -128,17 +204,11 @@
                     <p style="font-weight:bold">IMPORTANT</p>
                     <p>By submitting this form you are certifying that the amounts were expended while on Wye Valley Community Council business.</p>
                     <p>A PDF of the form will be produced and this should be saved and sent to the Clerk.</p>
-                    <input type="submit" id="printform" name="printform" value="Produce form" />
-                </div>
+                 </div>
             </form>
-        </div>
+            <input type="button" id="printform" name="printform" value="Produce form" />
+       </div>
     </body>
 </html>
-<script src="../js/jquery-3.1.1.min.js"></script>
-<script src="../js/jquery-ui.min.js"></script>
-<script>
-    $('#date').datepicker({
-        dateFormat: "d MM yy"
-    });
-</script>
+
 
